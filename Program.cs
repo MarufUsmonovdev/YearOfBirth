07d0fb2e0a5a9f1f904492398d5c533eb6d24f50
MarufUsmonovdev <142956091+MarufUsmonovdev@users.noreply.github.com>
2023-08-27 06:33:52 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter this year!");
+string thisYear = Console.ReadLine();
+
+System.Console.WriteLine("Enter year of birth!");
+string OwnYear = Console.ReadLine();
+
+int x = Convert.ToInt32(OwnYear);
+int Year = Convert.ToInt32(thisYear);
+
+int dayofbirth = Year - x;
+
+System.Console.WriteLine($"Your birth day is {dayofbirth * 365}");
